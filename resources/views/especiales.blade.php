a503ff9bcc9bc3b8e4913cf87a1639dd084e6c1d
LizAraLeonGarcia <lizbeth.lgarcia@alumnos.udg.mx>
2025-01-06 14:21:46 -0600

    correcciones Especiales

--- resources/views/especiales.blade.php
+++ resources/views/especiales.blade.php
@@ -10,28 +10,30 @@
     <div class="col-md-12">
     <!-- ------------------------------------------------- contendor  ------------------------------------------------- -->
       <ol>
-        <li data-bs-target="#" data-bs-slide-to="0" class="active">Evil Ryu</li>
-        <li data-bs-target="#" data-bs-slide-to="1">Violent Ken</li>
-        <li data-bs-target="#" data-bs-slide-to="2">Chun Li Player 2</li>
+        <li data-bs-target="swiper-slide" data-bs-slide-to="0" class="active">Evil Ryu</li>
+        <li data-bs-target="swiper-slide" data-bs-slide-to="7">Violent Ken</li>
+        <li data-bs-target="swiper-wrapper" data-bs-slide-to="14">Chun Li Player 2</li>
       </ol>
     </div>
     <!-- ---------------------------------------------------- contenedor para personaje ---------------------------------------------------- -->
-    <div class="especiales">
+    <div class="especiales" id="especiales">
       <!-- ------------------------------------------------------- Evil Ryu ------------------------------------------------------- -->
-      <div class="swiper-container swiper-container-evilryu">
+      <div class="swiper-container">
         <h4>Evil Ryu</h4>
         <div class="swiper-wrapper">
-          <div class="swiper-slide"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/evilryu1.jpg"><img src="assets/img/especiales/evilryu1.jpg" class="img-fluid" alt="Ryu frontal"></a></div>
-          <div class="swiper-slide"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/evilryu2.jpg"><img src="assets/img/especiales/evilryu2.jpg" class="img-fluid" alt="Ryu trasera"></a></div>
-          <div class="swiper-slide"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/evilryu3.jpg"><img src="assets/img/especiales/evilryu3.jpg" class="img-fluid" alt="Ryu dentro"></a></div>
-          <div class="swiper-slide"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/evilryu4.jpg"><img src="assets/img/especiales/evilryu4.jpg" class="img-fluid" alt="Ryu efecto 1"></a></div>
-          <div class="swiper-slide"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/evilryu5.jpg"><img src="assets/img/especiales/evilryu5.jpg" class="img-fluid" alt="Ryu efecto 2"></a></div>
-          <div class="swiper-slide"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/evilryu6.jpg"><img src="assets/img/especiales/evilryu6.jpg" class="img-fluid" alt="Ryu pose 1"></a></div>
-          <div class="swiper-slide"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/evilryu7.jpg"><img src="assets/img/especiales/evilryu7.jpg" class="img-fluid" alt="Ryu pose 2"></a></div>
+          <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/evilryu1.jpg"><img src="assets/img/especiales/evilryu1.jpg" class="img-fluid" alt="Ryu frontal"></a></div>
+          <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/evilryu2.jpg"><img src="assets/img/especiales/evilryu2.jpg" class="img-fluid" alt="Ryu trasera"></a></div>
+          <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/evilryu3.jpg"><img src="assets/img/especiales/evilryu3.jpg" class="img-fluid" alt="Ryu dentro"></a></div>
+          <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/evilryu4.jpg"><img src="assets/img/especiales/evilryu4.jpg" class="img-fluid" alt="Ryu efecto 1"></a></div>
+          <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/evilryu5.jpg"><img src="assets/img/especiales/evilryu5.jpg" class="img-fluid" alt="Ryu efecto 2"></a></div>
+          <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/evilryu6.jpg"><img src="assets/img/especiales/evilryu6.jpg" class="img-fluid" alt="Ryu pose 1"></a></div>
+          <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/evilryu7.jpg"><img src="assets/img/especiales/evilryu7.jpg" class="img-fluid" alt="Ryu pose 2"></a></div>
         </div>
+        <div class="button-next-evilryu"></div> <!-- botón para siguiente -->
+        <div class="button-prev-evilryu"></div> <!-- botón para previo --> 
       </div>
       <!-- ------------------------------------------------------- Violent Ken ------------------------------------------------------- -->
-      <div class="swiper-container swiper-container-violentken" style="display: none;">
+      <div class="swiper-container" style="display: none;">
         <h4>Violent Ken</h4>
         <div class="swiper-wrapper">
           <div class="swiper-slider"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/violentken1.jpg"><img src="assets/img/especiales/violentken1.jpg" class="img-fluid" alt="Violent Ken frontal"></a></div>
@@ -42,9 +44,11 @@
           <div class="swiper-slider"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/violentken6.jpg"><img src="assets/img/especiales/violentken6.jpg" class="img-fluid" alt="Violent Ken pose 1"></a></div>
           <div class="swiper-slider"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/violentken7.jpg"><img src="assets/img/especiales/violentken7.jpg" class="img-fluid" alt="Violent Ken pose 2"></a></div>
         </div>
+        <div class="swiper-button-next-violentken"></div> <!-- botón para siguiente -->
+        <div class="swiper-button-prev-violentken"></div> <!-- botón para previo --> 
       </div>
       <!-- ------------------------------------------------------- Chun Li ------------------------------------------------------- -->
-      <div class="swiper-container swiper-container-chunli" style="display: none;">
+      <div class="swiper-container" style="display: none;">
         <h4>Chun Li Player 2</h4>
         <div class="swiper-wrapper">
           <div class="swiper-slide"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/chunlirosa1.jpg"><img src="assets/img/especiales/chunlirosa1.jpg" class="img-fluid" alt="Chun Li frontal"></a></div>
@@ -55,9 +59,9 @@
           <div class="swiper-slide"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/chunlirosa6.jpg"><img src="assets/img/especiales/chunlirosa6.jpg" class="img-fluid" alt="Chun Li pose 1"></a></div>
           <div class="swiper-slide"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/chunlirosa7.jpg"><img src="assets/img/especiales/chunlirosa7.jpg" class="img-fluid" alt="Chun Li pose 2"></a></div>
         </div>
-      </div>
-      <div class="swiper-button-next"></div> <!-- botón para siguiente -->
-      <div class="swiper-button-prev"></div> <!-- botón para previo -->          
+        <div class="swiper-button-next-chunlirosa"></div> <!-- botón para siguiente -->
+        <div class="swiper-button-prev-chunlirosa"></div> <!-- botón para previo --> 
+      </div>         
     </div>
   </div>
 @endsection

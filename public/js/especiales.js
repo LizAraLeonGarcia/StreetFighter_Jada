7af1d499e85618b2ff3a6871ad3a5157e9326f1d
LizAraLeonGarcia <lizbeth.lgarcia@alumnos.udg.mx>
2025-01-07 14:56:31 -0600

    javascript para especiales

--- public/js/especiales.js
+++ public/js/especiales.js
@@ -1,29 +1,74 @@
 document.addEventListener('DOMContentLoaded', function () {
+  // Obtén las listas de personajes
+  const evilRyuItem = document.getElementById("evilryu");
+  const violentKenItem = document.getElementById("violentken");
+  const chunLiPlayer2Item = document.getElementById("chunlirosa");
+  // Obtén los contenedores correspondientes
+  const evilRyuContainer = document.querySelector(".evilryu-container");
+  const violentKenContainer = document.querySelector(".violentken-container");
+  const chunLiPlayer2Container = document.querySelector(".chunlirosa-container");
+  // Función para ocultar todos los contenedores
+  function hideAllContainers() {
+    evilRyuContainer.style.display = "none";
+    violentKenContainer.style.display = "none";
+    chunLiPlayer2Container.style.display = "none";
+  }
+  // Función para mostrar el contenedor seleccionado
+  function showContainer(container) {
+    hideAllContainers();
+    container.style.display = "block";
+  }
+  // Función para gestionar el cambio de la clase activa
+  function setActiveItem(item) {
+    // Remueve la clase active de todos los elementos
+    evilRyuItem.classList.remove('active');
+    violentKenItem.classList.remove('active');
+    chunLiPlayer2Item.classList.remove('active');
+    // Agrega la clase active al elemento seleccionado
+    item.classList.add('active');
+  }
+  // Asigna los eventos a cada elemento de la lista
+  evilRyuItem.addEventListener("click", function() {
+    showContainer(evilRyuContainer);
+    setActiveItem(evilRyuItem);
+  });
+
+  violentKenItem.addEventListener("click", function() {
+    showContainer(violentKenContainer);
+    setActiveItem(violentKenItem);
+  });
+
+  chunLiPlayer2Item.addEventListener("click", function() {
+    showContainer(chunLiPlayer2Container);
+    setActiveItem(chunLiPlayer2Item);
+  });
+  // Por defecto, mostrar Evil Ryu al cargar la página y establecerlo como activo
+  showContainer(evilRyuContainer);
+  setActiveItem(evilRyuItem);
+  // Inicializa Swiper
   var swiper = new Swiper('.swiper-container', {
-    loop: true,                // Para que el carrusel sea continuo
-    centeredSlides: true,      // Asegura que la primera imagen está centrada
-    initialSlide: 0,           // Empieza desde la primera imagen
-    spaceBetween: 10,          // Espacio entre imágenes
+    loop: true,
+    centeredSlides: true,
+    initialSlide: 0,
+    spaceBetween: 10,
     navigation: {
       nextEl: '.swiper-button-next',
       prevEl: '.swiper-button-prev',
     },
     pagination: {
       el: '.swiper-pagination',
-      clickable: true,         // Habilita la paginación clickable
+      clickable: true,
     },
-
-    loop: true, // Hacer que el carrusel sea cíclico
     breakpoints: {
       640: {
-        slidesPerView: 1, // En pantallas pequeñas, mostrar una imagen
+        slidesPerView: 1,
       },
       768: {
-        slidesPerView: 2, // En pantallas medianas, mostrar dos imágenes
+        slidesPerView: 2,
       },
       1024: {
-        slidesPerView: 3, // En pantallas grandes, mostrar tres imágenes
+        slidesPerView: 3,
       },
     },
-  });  
+  });
 });
--- resources/views/especiales.blade.php
+++ resources/views/especiales.blade.php
@@ -10,15 +10,15 @@
     <div class="col-md-12">
     <!-- ------------------------------------------------- contendor  ------------------------------------------------- -->
       <ol>
-        <li data-bs-target="swiper-slide" data-bs-slide-to="0" class="active">Evil Ryu</li>
-        <li data-bs-target="swiper-slide" data-bs-slide-to="7">Violent Ken</li>
-        <li data-bs-target="swiper-wrapper" data-bs-slide-to="14">Chun Li Player 2</li>
+        <li id="evilryu" class="active">Evil Ryu</li>
+        <li id="violentken">Violent Ken</li>
+        <li id="chunlirosa">Chun Li Player 2</li>
       </ol>
     </div>
     <!-- ---------------------------------------------------- contenedor para personaje ---------------------------------------------------- -->
     <div class="especiales" id="especiales">
       <!-- ------------------------------------------------------- Evil Ryu ------------------------------------------------------- -->
-      <div class="swiper-container">
+      <div class="evilryu-container swiper-container">
         <h4>Evil Ryu</h4>
         <div class="swiper-wrapper">
           <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/evilryu1.jpg"><img src="assets/img/especiales/evilryu1.jpg" class="img-fluid" alt="Ryu frontal"></a></div>
@@ -29,26 +29,26 @@
           <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/evilryu6.jpg"><img src="assets/img/especiales/evilryu6.jpg" class="img-fluid" alt="Ryu pose 1"></a></div>
           <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/evilryu7.jpg"><img src="assets/img/especiales/evilryu7.jpg" class="img-fluid" alt="Ryu pose 2"></a></div>
         </div>
-        <div class="button-next-evilryu"></div> <!-- botón para siguiente -->
-        <div class="button-prev-evilryu"></div> <!-- botón para previo --> 
+        <div class="swiper-button-next-especiales"></div> <!-- botón para siguiente -->
+        <div class="swiper-button-prev-especiales"></div> <!-- botón para previo --> 
       </div>
       <!-- ------------------------------------------------------- Violent Ken ------------------------------------------------------- -->
-      <div class="swiper-container" style="display: none;">
+      <div class="violentken-container swiper-container" style="display: none;">
         <h4>Violent Ken</h4>
         <div class="swiper-wrapper">
-          <div class="swiper-slider"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/violentken1.jpg"><img src="assets/img/especiales/violentken1.jpg" class="img-fluid" alt="Violent Ken frontal"></a></div>
-          <div class="swiper-slider"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/violentken2.jpg"><img src="assets/img/especiales/violentken2.jpg" class="img-fluid" alt="Violent Ken trasera"></a></div>
-          <div class="swiper-slider"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/violentken3.jpg"><img src="assets/img/especiales/violentken3.jpg" class="img-fluid" alt="Violent Ken dentro"></a></div>
-          <div class="swiper-slider"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/violentken4.jpg"><img src="assets/img/especiales/violentken4.jpg" class="img-fluid" alt="Violent Ken efecto 1"></a></div>
-          <div class="swiper-slider"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/violentken5.jpg"><img src="assets/img/especiales/violentken5.jpg" class="img-fluid" alt="Violent Ken efecto 2"></a></div>
-          <div class="swiper-slider"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/violentken6.jpg"><img src="assets/img/especiales/violentken6.jpg" class="img-fluid" alt="Violent Ken pose 1"></a></div>
-          <div class="swiper-slider"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/violentken7.jpg"><img src="assets/img/especiales/violentken7.jpg" class="img-fluid" alt="Violent Ken pose 2"></a></div>
+          <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/violentken1.jpg"><img src="assets/img/especiales/violentken1.jpg" class="img-fluid" alt="Violent Ken frontal"></a></div>
+          <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/violentken2.jpg"><img src="assets/img/especiales/violentken2.jpg" class="img-fluid" alt="Violent Ken trasera"></a></div>
+          <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/violentken3.jpg"><img src="assets/img/especiales/violentken3.jpg" class="img-fluid" alt="Violent Ken dentro"></a></div>
+          <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/violentken4.jpg"><img src="assets/img/especiales/violentken4.jpg" class="img-fluid" alt="Violent Ken efecto 1"></a></div>
+          <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/violentken5.jpg"><img src="assets/img/especiales/violentken5.jpg" class="img-fluid" alt="Violent Ken efecto 2"></a></div>
+          <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/violentken6.jpg"><img src="assets/img/especiales/violentken6.jpg" class="img-fluid" alt="Violent Ken pose 1"></a></div>
+          <div class="swiper-slide"><a data-especiales="images-especiales" href="assets/img/especiales/violentken7.jpg"><img src="assets/img/especiales/violentken7.jpg" class="img-fluid" alt="Violent Ken pose 2"></a></div>
         </div>
-        <div class="swiper-button-next-violentken"></div> <!-- botón para siguiente -->
-        <div class="swiper-button-prev-violentken"></div> <!-- botón para previo --> 
+        <div class="swiper-button-next-especiales"></div> <!-- botón para siguiente -->
+        <div class="swiper-button-prev-especiales"></div> <!-- botón para previo --> 
       </div>
       <!-- ------------------------------------------------------- Chun Li ------------------------------------------------------- -->
-      <div class="swiper-container" style="display: none;">
+      <div class="chunlirosa-container swiper-container" style="display: none;">
         <h4>Chun Li Player 2</h4>
         <div class="swiper-wrapper">
           <div class="swiper-slide"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/chunlirosa1.jpg"><img src="assets/img/especiales/chunlirosa1.jpg" class="img-fluid" alt="Chun Li frontal"></a></div>
@@ -59,8 +59,8 @@
           <div class="swiper-slide"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/chunlirosa6.jpg"><img src="assets/img/especiales/chunlirosa6.jpg" class="img-fluid" alt="Chun Li pose 1"></a></div>
           <div class="swiper-slide"><a class="glightbox" data-especiales="images-especiales" href="assets/img/especiales/chunlirosa7.jpg"><img src="assets/img/especiales/chunlirosa7.jpg" class="img-fluid" alt="Chun Li pose 2"></a></div>
         </div>
-        <div class="swiper-button-next-chunlirosa"></div> <!-- botón para siguiente -->
-        <div class="swiper-button-prev-chunlirosa"></div> <!-- botón para previo --> 
+        <div class="swiper-button-next-especiales"></div> <!-- botón para siguiente -->
+        <div class="swiper-button-prev-especiales"></div> <!-- botón para previo --> 
       </div>         
     </div>
   </div>

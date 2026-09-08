5b97078bbc7cfae05fbc2c413b8b69e60bc991f8
LizAraLeonGarcia <lizbeth.lgarcia@alumnos.udg.mx>
2026-09-08 13:52:17 -0600

    actualizacion del menu o navbar

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -2,44 +2,38 @@
     <div class="navbarIMGizquierda">
         <img src="{{ asset('assets/img/navbar/1.png') }}" alt="Chun Li rosa" class="img-fluid">
     </div>
-
     <ul class="nav-list">
-        <li class="nav-item">
-            <a href="{{ route('inicio') }}"
-               class="nav-link {{ request()->routeIs('inicio') ? 'active' : '' }}">
+        <li class="nav-item"> <!-- ------------------------------------------------------------------------------------------------- Inicio -->
+            <a href="{{ route('inicio') }}" class="nav-link {{ request()->routeIs('inicio') ? 'active' : '' }}">
                 Inicio
             </a>
         </li>
-
-        <li class="nav-item">
-            <a href="{{ route('personajes') }}"
-               class="nav-link {{ request()->routeIs('personajes') ? 'active' : '' }}">
+        <li class="nav-item"> <!-- --------------------------------------------------------------------------------------------- Personajes -->
+            <a href="{{ route('personajes') }}" class="nav-link {{ request()->routeIs('personajes') ? 'active' : '' }}">
                 Personajes
             </a>
         </li>
-
-        <li class="nav-item">
-            <a href="{{ route('especiales') }}"
-               class="nav-link {{ request()->routeIs('especiales') ? 'active' : '' }}">
+        <li class="nav-item"> <!-- --------------------------------------------------------------------------------------------- Especiales -->
+            <a href="{{ route('especiales') }}" class="nav-link {{ request()->routeIs('especiales') ? 'active' : '' }}">
                 Especiales
             </a>
         </li>
-
-        <li class="nav-item">
-            <a href="{{ route('variantes') }}"
-               class="nav-link {{ request()->routeIs('variantes') ? 'active' : '' }}">
+        <li class="nav-item"> <!-- ---------------------------------------------------------------------------------------------- Variantes -->
+            <a href="{{ route('variantes') }}" class="nav-link {{ request()->routeIs('variantes') ? 'active' : '' }}">
                 Variantes
             </a>
         </li>
-
-        <li class="nav-item">
-            <a href="{{ route('galeria') }}"
-               class="nav-link {{ request()->routeIs('galeria') ? 'active' : '' }}">
+        <li class="nav-item"> <!-- ------------------------------------------------------------------------------------------------ Galería -->
+            <a href="{{ route('galeria') }}" class="nav-link {{ request()->routeIs('galeria') ? 'active' : '' }}">
                 Galería
             </a>
         </li>
+        <li class="nav-item"> <!-- -------------------------------------------------------------------------------------------------- Alpha -->
+            <a href="{{ route('alpha') }}" class="nav-link {{ request()->routeIs('alpha') ? 'active' : '' }}">
+                Alpha
+            </a>
+        </li>
     </ul>
-
     <div class="navbarIMGderecha">
         <img src="{{ asset('assets/img/navbar/2.png') }}" alt="Chun Li Clásico" class="img-fluid">
     </div>
